010c53526ce1119f95cb891a9530b65a46980490
Xu Jiandong <xjd@cryptape.com>
2020-09-21 17:47:00 +0800

    use format_xxx! macro

--- src/generator.rs
+++ src/generator.rs
@@ -2,9 +2,28 @@ use crate::utils::{Output, generate};
 use core::mem::size_of;
 use molecule_codegen::ast::{self, DefaultContent, HasName, *};
 use std::fmt::{Write, Result};
+use std::fmt;
 
-pub type Number = u32;
-pub const NUMBER_SIZE: usize = size_of::<Number>();
+macro_rules! format_decl {
+    ($output:expr, $($arg:tt)*) => {{
+        let res = fmt::format(format_args!($($arg)*));
+        $output.write_decl(&res);
+    }}
+}
+
+macro_rules! format_def {
+    ($output:expr, $($arg:tt)*) => {{
+        let res = fmt::format(format_args!($($arg)*));
+        $output.write_def(&res);
+    }}
+}
+
+macro_rules! format_imp {
+    ($output:expr, $($arg:tt)*) => {{
+        let res = fmt::format(format_args!($($arg)*));
+        $output.write_imp(&res);
+    }}
+}
 
 pub trait Generator: HasName + DefaultContent {
     fn generate(&self, output: &mut Output) -> Result;
@@ -60,85 +79,83 @@ impl Generator for ast::DynVec {
     fn generate(&self, output: &mut Output) -> Result {
         let item_type = get_dynvec_item_type(self);
         let name = self.name();
-        output.write_decl(&format!("struct {};", gen_class_name(name)));
-        output.write_decl(&format!("struct {}VTable;", name));
-        output.write_decl(&format!("struct {}VTable *Get{}VTable(void);", name, name));
+        format_decl!(output, "struct {};", gen_class_name(name));
+        format_decl!(output, "struct {}VTable;", name);
+        format_decl!(output, "struct {}VTable *Get{}VTable(void);", name, name);
 
-        output.write_decl(&format!("uint32_t {0}_len_impl(struct {0}Type *);", name));
-        output.write_decl(&format!("struct {0} {1}_get_impl(struct {1}Type *, uint32_t, int *);", item_type, name));
+        format_decl!(output, "uint32_t {0}_len_impl(struct {0}Type *);", name);
+        format_decl!(output, "struct {0} {1}_get_impl(struct {1}Type *, uint32_t, int *);", item_type, name);
         // --------- declaration above ------------
 
         // ----------definition below -------------
         // definition of virtual table
-        output.write_def(&format!("typedef struct {0}VTable {{", name));
+        format_def!(output, "typedef struct {0}VTable {{", name);
 
-        output.write_def(&format!("uint32_t (*len)(struct {}Type *);", name));
-        output.write_def(&format!("{1} (*get)(struct {0}Type *, uint32_t, int *);", name, item_type));
+        format_def!(output, "uint32_t (*len)(struct {}Type *);", name);
+        format_def!(output, "{1} (*get)(struct {0}Type *, uint32_t, int *);", name, item_type);
 
-        output.write_def(&format!("}} {}VTable;", name));
+        format_def!(output, "}} {}VTable;", name);
         // definition of class
-        output.write_def(&format!(r#"typedef struct {0}Type {{
-    mol2_cursor_t cur;
-    {0}VTable *tbl;
-    }} {0}Type;
-    "#, name));
+        format_def!(output, r#"typedef struct {0}Type {{
+        mol2_cursor_t cur;
+        {0}VTable *tbl;
+        }} {0}Type;
+        "#, name);
         // definition of "make" class instance
-        output.write_def(&format!(r#"struct {0}Type make_{0}(mol2_cursor_t *cur) {{
-    {0}Type ret;
-    ret.cur = *cur;
-    ret.tbl = Get{0}VTable();
-    return ret;
-              }}
-              "#, name));
-        // definition of "get" class vtable
-        output.write_def(&format!(r#"struct {0}VTable *Get{0}VTable(void) {{
-    static {0}VTable s_vtable;
-    static int inited = 0;
-    if (inited) return &s_vtable;
-            "#, name));
-
-        output.write_def(&format!("s_vtable.len = {}_len_impl;", name));
-        output.write_def(&format!("s_vtable.get = {}_get_impl;", name));
-
-        output.write_def(&format!("return &s_vtable; }}"));
-
-        // entries of virtual tables
-        output.write_imp(&format!(r#"
-        uint32_t {0}_len_impl({0}Type *this) {{
-          return mol2_dynvec_length(&this->cur);
-        }}"#, name));
-
-        if item_type == "mol2_cursor_t" {
-            output.write_imp(&format!(r#"
-    mol2_cursor_t {0}_get_impl({0}Type *this,
-                            uint32_t index, int *existing) {{
-    mol2_cursor_t ret;
-    mol2_cursor_res_t res = mol2_dynvec_slice_by_index(&this->cur, index);
-    if (res.errno != 0) {{
-        *existing = 0;
-        return ret;
-    }} else {{
-        *existing = 1;
-    }}
-    ret = convert_to_rawbytes(&res.cur);
-    return ret;
-    }}"#, name));
-        } else {
-            output.write_imp(&format!(r#"
-    {1}Type {0}_get_impl({0}Type *this,
-                            uint32_t index, int *existing) {{
-    {0}Type ret;
-    mol2_cursor_res_t res = mol2_dynvec_slice_by_index(&this->cur, index);
-    if (res.errno != 0) {{
-        *existing = 0;
-        return ret;
-    }} else {{
-        *existing = 1;
-    }}
-        ret.cur = res.cur;
+        format_def!(output, r#"struct {0}Type make_{0}(mol2_cursor_t *cur) {{
+        {0}Type ret;
+        ret.cur = *cur;
         ret.tbl = Get{0}VTable();
         return ret;
-    }}"#, name, item_type));
+        }}"#, name);
+        // definition of "get" class vtable
+        format_def!(output, r#"struct {0}VTable *Get{0}VTable(void) {{
+        static {0}VTable s_vtable;
+        static int inited = 0;
+        if (inited) return &s_vtable; "#, name);
+
+        format_def!(output, "s_vtable.len = {}_len_impl;", name);
+        format_def!(output, "s_vtable.get = {}_get_impl;", name);
+
+        format_def!(output, "return &s_vtable; }}");
+
+        // entries of virtual tables
+        format_imp!(output, r#"
+        uint32_t {0}_len_impl({0}Type *this) {{
+          return mol2_dynvec_length(&this->cur);
+        }}"#, name);
+
+        if item_type == "mol2_cursor_t" {
+            format_imp!(output, r#"
+            mol2_cursor_t {0}_get_impl({0}Type *this,
+                                    uint32_t index, int *existing) {{
+            mol2_cursor_t ret;
+            mol2_cursor_res_t res = mol2_dynvec_slice_by_index(&this->cur, index);
+            if (res.errno != 0) {{
+                *existing = 0;
+                return ret;
+            }} else {{
+                *existing = 1;
+            }}
+            ret = convert_to_rawbytes(&res.cur);
+            return ret;
+            }}"#, name);
+        } else {
+            format_imp!(output, r#"
+            {1}Type {0}_get_impl({0}Type *this,
+                                    uint32_t index, int *existing) {{
+            {0}Type ret;
+            mol2_cursor_res_t res = mol2_dynvec_slice_by_index(&this->cur, index);
+            if (res.errno != 0) {{
+                *existing = 0;
+                return ret;
+            }} else {{
+                *existing = 1;
+            }}
+                ret.cur = res.cur;
+                ret.tbl = Get{0}VTable();
+                return ret;
+            }}"#, name, item_type);
         }
         Ok(())
     }
@@ -159,53 +176,52 @@ fn need_struct_prefix(field_type: &str) -> String {
 // Table/Struct
 fn generate_common(output: &mut Output, name: &str,
                    fields: &[FieldDecl], field_sizes: Option<&[usize]>) -> Result {
-    output.write_decl(&format!("struct {};", gen_class_name(name)));
-    output.write_decl(&format!("struct {}VTable;", name));
-    output.write_decl(&format!("struct {}VTable *Get{}VTable(void);", name, name));
+    format_decl!(output, "struct {};", gen_class_name(name));
+    format_decl!(output, "struct {}VTable;", name);
+    format_decl!(output, "struct {}VTable *Get{}VTable(void);", name, name);
 
     for field in fields {
         let field_name = &field.name();
         let (field_type, _) = gen_field_type(field);
-        output.write_decl(&format!("{0} {1}_get_{2}_impl(struct {1}Type *);",
-                                   need_struct_prefix(&field_type), name, field_name));
+        format_decl!(output, "{0} {1}_get_{2}_impl(struct {1}Type *);",
+                                   need_struct_prefix(&field_type), name, field_name);
     }
     // --------- declaration above ------------
 
     // ----------definition below -------------
     // definition of virtual table
-    output.write_def(&format!("typedef struct {0}VTable {{", name));
+    format_def!(output, "typedef struct {0}VTable {{", name);
     for field in fields {
         let field_name = &field.name();
         let (field_type, _) = gen_field_type(field);
-        output.write_def(&format!("{0} (*{1})(struct {2}Type *);", field_type, field_name, name));
+        format_def!(output, "{0} (*{1})(struct {2}Type *);", field_type, field_name, name);
     }
-    output.write_def(&format!("}} {}VTable;", name));
+    format_def!(output, "}} {}VTable;", name);
     // definition of class
-    output.write_def(&format!(r#"typedef struct {0}Type {{
+    format_def!(output, r#"typedef struct {0}Type {{
     mol2_cursor_t cur;
     {0}VTable *tbl;
     }} {0}Type;
-    "#, name));
+    "#, name);
     // definition of "make" class instance
-    output.write_def(&format!(r#"struct {0}Type make_{0}(mol2_cursor_t *cur) {{
+    format_def!(output, r#"struct {0}Type make_{0}(mol2_cursor_t *cur) {{
     {0}Type ret;
     ret.cur = *cur;
     ret.tbl = Get{0}VTable();
     return ret;
-              }}
-              "#, name));
+    }}"#, name);
     // definition of "get" class vtable
-    output.write_def(&format!(r#"struct {0}VTable *Get{0}VTable(void) {{
+    format_def!(output, r#"struct {0}VTable *Get{0}VTable(void) {{
     static {0}VTable s_vtable;
     static int inited = 0;
     if (inited) return &s_vtable;
-            "#, name));
+    "#, name);
     for field in fields {
         let field_name = &field.name();
         let (field_type, _) = gen_field_type(field);
-        output.write_def(&format!("s_vtable.{0} = {1}_get_{0}_impl;", field_name, name));
+        format_def!(output, "s_vtable.{0} = {1}_get_{0}_impl;", field_name, name);
     }
-    output.write_def(&format!("return &s_vtable; }}"));
+    format_def!(output, "return &s_vtable; }}");
     // entries of virtual tables
     let mut index : usize = 0;
     for field in fields {
@@ -222,32 +238,32 @@ fn generate_impl(output: &mut Output, name: &str, field: &FieldDecl,
     let slice_by = generate_slice_by(index, &field_sizes);
     match ftc {
         FieldTypeCategory::Type => {
-            output.write_imp(&format!(r#"
-    {2} {0}_get_{1}_impl({0}Type *this) {{
-    {2} ret;
-    mol2_cursor_t cur = {4};
-    ret.cur = cur;
-    ret.tbl = Get{3}VTable();
-    return ret;
-    }}"#, name, field_name, field_type, raw_name(&field_type), slice_by));
+            format_imp!(output, r#"
+            {2} {0}_get_{1}_impl({0}Type *this) {{
+            {2} ret;
+            mol2_cursor_t cur = {4};
+            ret.cur = cur;
+            ret.tbl = Get{3}VTable();
+            return ret;
+            }}"#, name, field_name, field_type, raw_name(&field_type), slice_by);
         },
         FieldTypeCategory::Primitive|FieldTypeCategory::Array => {
-            output.write_imp(&format!(r#"
-    {2} {0}_get_{1}_impl({0}Type *this) {{
-    {2} ret;
-    mol2_cursor_t ret2 = {3};
-    ret = {4}(&ret2);
-    return ret;
-    }}"#, name, field_name, field_type, slice_by, get_convert_func(&field_type, &ftc)));
+            format_imp!(output, r#"
+            {2} {0}_get_{1}_impl({0}Type *this) {{
+            {2} ret;
+            mol2_cursor_t ret2 = {3};
+            ret = {4}(&ret2);
+            return ret;
+            }}"#, name, field_name, field_type, slice_by, get_convert_func(&field_type, &ftc));
         },
         FieldTypeCategory::FixVec => {
-            output.write_imp(&format!(r#"
-    {2} {0}_get_{1}_impl({0}Type *this) {{
-    mol2_cursor_t ret;
-    mol2_cursor_t re2 = {3};
-    ret = convert_to_rawbytes(&re2);
-    return ret;
-    }}"#, name, field_name, field_type, slice_by));
+            format_imp!(output, r#"
+            {2} {0}_get_{1}_impl({0}Type *this) {{
+            mol2_cursor_t ret;
+            mol2_cursor_t re2 = {3};
+            ret = convert_to_rawbytes(&re2);
+            return ret;
+            }}"#, name, field_name, field_type, slice_by);
         },
     }
 }
